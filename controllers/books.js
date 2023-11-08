342875acd7c7ecae3a93c1c58f895cbe6ad85b84
Vani Battu <vani.unf87@gmail.com>
2023-11-08 12:38:27 -0600

    final

--- controllers/books.js
+++ controllers/books.js
@@ -29,9 +29,26 @@ exports.books_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: books detail: ' + req.params.id);
 };
 // Handle books create on POST.
-exports.books_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: books create POST');
-};
+exports.books_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new books();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.name = req.body.name;
+    document.author = req.body.author;
+    document.cost = req.body.cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
 // Handle books delete form on DELETE.
 exports.books_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: books delete DELETE ' + req.params.id);
