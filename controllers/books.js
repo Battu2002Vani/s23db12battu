a3f9183106f94674b5d0fc996d1ad42643d30def
Vani Battu <vani.unf87@gmail.com>
2023-11-09 22:21:31 -0600

    labdone

--- controllers/books.js
+++ controllers/books.js
@@ -68,6 +68,26 @@ exports.books_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: books delete DELETE ' + req.params.id);
 };
 // Handle books update form on PUT.
-exports.books_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: books update PUT' + req.params.id);
+//exports.books_update_put = function(req, res) {
+//res.send('NOT IMPLEMENTED: books update PUT' + req.params.id);
+//};
+// Handle  update form on PUT.
+exports.books_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await books.findById( req.params.id)
+// Do updates of properties
+if(req.body.books_type)
+toUpdate.name = req.body.name;
+if(req.body.author) toUpdate.author = req.body.author;
+if(req.body.cost) toUpdate.cost = req.body.cost;
+let result = await toUpdate.save();
+console.log("Success " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
