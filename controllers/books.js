6882ec2a957bbdea7c66e191a4e18733e3254212
Vani Battu <vani.unf87@gmail.com>
2023-11-29 10:46:26 -0600

    assign13

--- controllers/books.js
+++ controllers/books.js
@@ -142,6 +142,20 @@ res.status(500)
 res.send(`{'error': '${err}'}`);
 }
 };
+// Handle a delete one view with id from query
+exports.books_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await books.findById(req.query.id)
+    res.render('booksdelete', { title: 'books Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
 
 
     
--- routes/Books.js
+++ routes/Books.js
@@ -1,6 +1,14 @@
 var express = require('express');
 const books_controlers= require('../controllers/books');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 /* GET books */
 router.get('/', books_controlers.books_view_all_Page );
 /* GET detail books page */
@@ -8,6 +16,8 @@ router.get('/detail', books_controlers.books_view_one_Page);
 /* GET create books page */
 router.get('/create', books_controlers.books_create_Page);
 /* GET create update page */
-router.get('/update', books_controlers.books_update_Page);
+router.get('/update',secured, books_controlers.books_update_Page);
+/* GET delete books page */
+router.get('/delete', books_controlers.books_delete_Page);
 
 module.exports = router;
